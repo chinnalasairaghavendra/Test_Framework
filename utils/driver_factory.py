1a2dd9651bdaa521fa8a8368c9799f2cb213eb5d
Raghavendra <chinnalasairaghavendra@gmail.com>
2026-03-16 14:53:45 +0530

    made small changes

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from config.settings import Config
 
 
@@ -7,21 +8,27 @@ def get_driver():
 
     options = Options()
 
+    # Headless mode for CI environments
     if Config.HEADLESS:
         options.add_argument("--headless=new")
+
+    # Required for Docker / Jenkins
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--remote-debugging-port=9222")
     options.add_argument("--window-size=1920,1080")
 
+    # If running on Selenium Grid
     if Config.GRID_URL:
-
         driver = webdriver.Remote(
             command_executor=Config.GRID_URL,
             options=options
         )
 
+    # Run locally
     else:
-
-        driver = webdriver.Chrome(options=options)
+        service = Service()   # Selenium manager auto handles chromedriver
+        driver = webdriver.Chrome(service=service, options=options)
 
     return driver
